3ecc833b4ca357e8b4fdd1a55b8a91c6a6563ff8
Hemlock4414 <antonio.marrara@gmx.ch>
2024-09-11 17:00:16 +0200

    new branch

---
+++ resources/js/views/FormView.vue
@@ -0,0 +1,22 @@
+
+<template>
+
+    <header>
+
+        <div class="headline">MINI-TWITTER</div>
+
+        <div>
+            <div class="headsmall">Meine Tweets</div>
+
+            <button class="erstellen">+ Tweet erstellen</button>
+        </div>    
+
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+
+</template>
